05a85d8a7fcab2807d92182c9bd639d804f97c9e
Hao Yu Yeh <AngusYeh@my--Mac-1267.local>
2017-12-12 22:01:23 +1100

    add multiple languages comparision method

--- UsefulMethods.playground/Contents.swift
+++ UsefulMethods.playground/Contents.swift
@@ -2,4 +2,24 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// sort by current locale
+let str1 = ["Hello", "a playground"]
+let str2 = ["我", "小"]
+
+let sortStr1 = str1.sorted { (s1, s2) -> Bool in
+    return s1.localizedStandardCompare(s2) == .orderedAscending
+}
+class item{
+    var name:String
+    init(name: String) {
+        self.name = name
+    }
+}
+var item2 = [item]()
+item2.append(item(name:"我"))
+item2.append(item(name:"小"))
+let sortItem2 = item2.sorted { (s1, s2) -> Bool in
+    return s1.name.localizedStandardCompare(s2.name) == .orderedAscending
+}
+print(sortItem2[0].name)
+
