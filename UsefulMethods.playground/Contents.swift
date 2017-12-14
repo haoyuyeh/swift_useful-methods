812207f4d313e2b44b22d7306a4d0161ddd94c43
Hao Yu Yeh <AngusYeh@my--Mac-1267.local>
2017-12-14 19:57:05 +1100

    add methods of passing data

--- UsefulMethods.playground/Contents.swift
+++ UsefulMethods.playground/Contents.swift
@@ -2,6 +2,8 @@
 
 import UIKit
 
+// MARK: Array sorting
+
 // sort by current locale
 let str1 = ["Hello", "a playground"]
 let str2 = ["我", "小"]
@@ -53,3 +55,91 @@ func testSort(target: Array<item>?, target2: Array<Folder>?) -> Array<item>{
 
 let t = testSort(target: item2, target2: nil)
 print("\(t[0].name) \(t[0].date)")
+
+
+// MARK: pass data from Model to Controller
+
+class ViewController: UIViewController {
+    private let dataModel = DataModel()
+    
+    // 1-1. Callback as Completion Handler
+    func callbackByCompletion() {
+        dataModel.requestData { [weak self](data: String) in
+            self?.useData(data: data)
+        }
+    }
+    // 1-2. Callback as a class property
+    func callbackByClassProperty() {
+        dataModel.onDataUpdate = { [weak self] (data: String) in
+            self?.useData(data: data)
+        }
+        dataModel.requestData()
+    }
+    
+    private func useData(data: String) {
+        print(data)
+    }
+    
+    // 2. Delegation
+    func passByDelegation() {
+        dataModel.delegate = self
+        dataModel.requestDataByDelegate()
+    }
+}
+extension ViewController: DataModelDelegate {
+    func didReceiveDataUpdate(data: String) {
+        print(data)
+    }
+}
+
+
+
+
+protocol DataModelDelegate: class {
+    func didReceiveDataUpdate(data: String)
+}
+class DataModel {
+    // 1-1. Callback as Completion Handler
+    func requestData(completion: ((_ data: String) -> Void)) {
+        // data type should be changed
+        let data = "Data from wherever"
+        completion(data)
+    }
+    // 1-2. Callback as a class property
+    var onDataUpdate: ((_ data: String) -> Void)?
+    func requestData() {
+        // data type should be changed
+        let data = "Data from wherever"
+        onDataUpdate!(data)
+    }
+    
+    // 2. Delegation
+    weak var delegate: DataModelDelegate?
+    func requestDataByDelegate() {
+        let data = "Data from wherever"
+        delegate?.didReceiveDataUpdate(data: data)
+    }
+}
+
+let view = ViewController()
+view.callbackByCompletion()
+view.callbackByClassProperty()
+view.passByDelegation()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
